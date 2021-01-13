a938105afe06f1e2fb08ea17855cfb56635e734b
Levi Kohler <kohler.la01@gmail.com>
2021-01-12 20:41:02 -0800

    use the same format for 4 more words/definitions

--- js/scripts.js
+++ js/scripts.js
@@ -23,4 +23,24 @@ $(document).ready(function() {
     $("#function-definition").toggle();
     $("#hide-function").toggle();
   });
+
+  $("#method").click(function() {
+    $("#method-definition").toggle();
+    $("#hide-method").toggle();
+  });
+
+  $("#argument").click(function() {
+    $("#argument-definition").toggle();
+    $("#hide-argument").toggle();
+  });
+
+  $("#parameter").click(function() {
+    $("#parameter-definition").toggle();
+    $("#hide-parameter").toggle();
+  });
+
+  $("#return").click(function() {
+    $("#return-definition").toggle();
+    $("#hide-return").toggle();
+  });
 });
